7579c324137225782be3defd64be1d3c5d48fc34
Jude Rozario <juderzro08@gmail.com>
2025-04-10 23:14:03 -0400

    simple change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { BottomTab } from './navigation/HomeTabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from './theming/ThemeProvider';
 
 export function App() {
     return (
         <ThemeProvider>
+            <NavigationContainer>
+                <BottomTab />
+            </NavigationContainer>
         </ThemeProvider>
     );
 }
