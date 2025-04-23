de69129b142fc61700db8ab7d3e3655d5bd6192c
Jude Rozario <juderzro08@gmail.com>
2025-04-22 21:03:18 -0400

    defining constants

---
+++ client/src/lib/constants.ts
@@ -0,0 +1,24 @@
+export const Months: string[] = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+]
+
+export const Days: string[] = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+]
