d0b78e6e857d1305543686048dcea0e9c2aebc82
Jude Rozario <juderzro08@gmail.com>
2025-06-17 09:40:00 -0400

    simplifying pressable

--- client/components/FadingPressable.tsx
+++ client/components/FadingPressable.tsx
@@ -1,15 +1,8 @@
-import { ReactNode } from "react";
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
-import { GestureResponderEvent, PressableStateCallbackType, StyleProp, ViewStyle } from "react-native/types";
+import { PressableProps } from "react-native/types";
 import { Pressable } from 'react-native';
 
-export const FadingPressable: React.FC<{
-    children: ReactNode;
-    style?:
-    | StyleProp<ViewStyle>
-    | ((state: PressableStateCallbackType) => StyleProp<ViewStyle>)
-    onPress: ((event: GestureResponderEvent) => void) | null | undefined
-}> = ({ children, style, onPress }) => {
+export const FadingPressable: React.FC<PressableProps> = (props) => {
     const opacity = useSharedValue(1);
     const opacityAnimatedStyle = useAnimatedStyle(() => ({
         opacity: opacity.value
@@ -17,16 +10,12 @@ export const FadingPressable: React.FC<{
     return (
         <Animated.View style={opacityAnimatedStyle}>
             <Pressable
-                style={style}
-                onPressIn={() => {
-                    opacity.value = withTiming(0.4, { duration: 100 });
-                }}
-                onPressOut={() => {
-                    opacity.value = withTiming(1, { duration: 100 });
-                }}
-                onPress={onPress}
+                style={props.style}
+                onPressIn={() => opacity.value = withTiming(0.4, { duration: 100 })}
+                onPressOut={() => opacity.value = withTiming(1, { duration: 100 })}
+                onPress={props.onPress}
             >
-                {children}
+                {props.children}
             </Pressable>
         </Animated.View>
     )
